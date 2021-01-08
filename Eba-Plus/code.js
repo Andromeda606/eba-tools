21e9e173da0c9246cd52a1693b4abb9a305c7278
Andromeda606 <43077625+Andromeda606@users.noreply.github.com>
2021-01-08 18:57:58 +0300

    Update code.js

--- Eba-Plus/code.js
+++ Eba-Plus/code.js
@@ -1,7 +1,10 @@
-if(document.getElementById("joinMeeting").classList.contains("disabled")){
-    document.getElementById("joinMeeting").classList.remove("disabled");
-    document.getElementsByClassName("live-active")[1].innerHTML="<h2 class=\"text-success dis-none\" id=\"process-started\" style=\"display: block;\"><i class=\"fas fa-exclamation-circle\"></i> Canlı\n					dersin başladı.</h2>\n				<h2 class=\"text-warning dis-none\" id=\"process-future\" style=\"display: none;\"> Sıradaki canlı dersine <span id=\"timer\"></span>\n					kaldı.</h2>";
-    //Canlı Ders başlamadı.
-}
-document.getElementById("active-brand-button").style = "display:inline-block";
-document.getElementById("active-brand-button").innerHTML = "Akademik desteğe giriş yap";
+setTimeout(function() {
+    if (document.getElementById("joinMeeting").classList.contains("disabled")) {
+        document.getElementById("joinMeeting").classList.remove("disabled");
+        document.getElementsByClassName("live-active")[1].innerHTML = "<h2 class=\"text-success dis-none\" id=\"process-started\" style=\"display: block;\"><i class=\"fas fa-exclamation-circle\"></i> Canlı\n					dersin başladı.</h2>\n				<h2 class=\"text-warning dis-none\" id=\"process-future\" style=\"display: none;\"> Sıradaki canlı dersine <span id=\"timer\"></span>\n					kaldı.</h2>";
+        //Canlı Ders başlamadı.
+    }
+    document.getElementById("active-brand-button").style = "display:inline-block";
+    document.getElementById("active-brand-button").innerHTML = "Akademik desteğe giriş yap";
+
+}, 200);
