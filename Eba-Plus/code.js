b2b9579c12e9993562fc5628289e4fd00dc32b1b
Andromeda606 <43077625+Andromeda606@users.noreply.github.com>
2021-01-08 18:37:57 +0300

    Update code.js

--- Eba-Plus/code.js
+++ Eba-Plus/code.js
@@ -1,6 +1,6 @@
 if(document.getElementById("joinMeeting").classList.contains("disabled")){
     document.getElementById("joinMeeting").classList.remove("disabled");
-    document.getElementsByClassName("live-active")[1].innerHTML="<h2 class=\"text-success dis-none\" id=\"process-started\" style=\"display: block;\"><i class=\"fas fa-exclamation-circle\"></i> Canlı\n					dersin başlamadı. Ama erkenden girebilirsin.</h2>\n				<h2 class=\"text-warning dis-none\" id=\"process-future\" style=\"display: none;\"> Sıradaki canlı dersine <span id=\"timer\"></span>\n					kaldı.</h2>";
+    document.getElementsByClassName("live-active")[1].innerHTML="<h2 class=\"text-success dis-none\" id=\"process-started\" style=\"display: block;\"><i class=\"fas fa-exclamation-circle\"></i> Canlı\n					dersin başladı.</h2>\n				<h2 class=\"text-warning dis-none\" id=\"process-future\" style=\"display: none;\"> Sıradaki canlı dersine <span id=\"timer\"></span>\n					kaldı.</h2>";
     //Canlı Ders başlamadı.
 }
 $("#active-brand-button").show();
